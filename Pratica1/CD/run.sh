c538a3bc93480c21cd8d87d24abf1800097c9da9
DrPunpun <pedrooliveira99@ua.pt>
2019-10-06 15:35:37 +0100

    dockerfile for CD project started, but with errors

--- Pratica1/CD/run.sh
+++ Pratica1/CD/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 gnome-terminal --title="Simulation" --geometry 60x25+0+0 -e "python3 simulation.py"
+sleep 3
 
 for i in {1..10} 
 do
 	port=$((5010+$i))
-	gnome-terminal --title="Cliente $i" -e "python3 client.py -p $port -t 420"
+	gnome-terminal --title="Cliente $i" -e "python3 client.py -p $port -t 600"
 done
