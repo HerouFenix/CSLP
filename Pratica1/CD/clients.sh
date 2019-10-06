f2ab969acf8f39e9ff1f9e1f609dfb783d6c0ee6
DrPunpun <pedrooliveira99@ua.pt>
2019-10-06 01:10:02 +0100

    merged conflicts

--- Pratica1/CD/clients.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-gnome-terminal --title="Ring" --geometry 60x25+0+0 -e "python3 simulation.py" & 
-sleep 3
-
-for i in {1..20} 
-do
-	port=$((5010+$i))
-	gnome-terminal --title="Cliente $i" -e "python3 client.py -p $port -t 600" &
-done
